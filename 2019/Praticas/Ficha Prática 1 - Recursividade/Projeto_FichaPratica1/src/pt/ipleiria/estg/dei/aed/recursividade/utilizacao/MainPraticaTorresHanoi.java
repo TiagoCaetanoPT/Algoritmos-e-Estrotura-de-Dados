4b8ff61873f03c9e9aba4b86950f743aa5a0d49f
unknown <tiago.o.caetano@gmail.com>
2019-10-04 16:26:53 +0100

    Torres Hanoi sem Estatisticas

--- 2019/Praticas/Ficha Prática 1 - Recursividade/Projeto_FichaPratica1/src/pt/ipleiria/estg/dei/aed/recursividade/utilizacao/MainPraticaTorresHanoi.java
+++ 2019/Praticas/Ficha Prática 1 - Recursividade/Projeto_FichaPratica1/src/pt/ipleiria/estg/dei/aed/recursividade/utilizacao/MainPraticaTorresHanoi.java
@@ -3,9 +3,12 @@ package pt.ipleiria.estg.dei.aed.recursividade.utilizacao;
 import pt.ipleiria.estg.dei.aed.recursividade.algoritmos.TorresHanoi;
 
 public class MainPraticaTorresHanoi {
-    public MainPraticaTorresHanoi(){
+    public MainPraticaTorresHanoi() {
+        int[] numeroDiscos = {3, 5, 10, 15, 30, 31, 32};
         TorresHanoi torresHanoi = new TorresHanoi();
-        torresHanoi.executar(3, 'A', 'B', 'C');
+        for (int n : numeroDiscos) {
+            torresHanoi.executar(n, 'A', 'B', 'C');
+        }
     }
 
     public static void main(String[] args) {
